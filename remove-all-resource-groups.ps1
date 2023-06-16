0d56f14e8566477058b644c15e5e2b6df5458b16
Bogdan Grozoiu <50367646+bogdan-grozoiu@users.noreply.github.com>
2023-06-16 13:36:45 +0200

    Update remove-all-resource-groups.ps1

--- remove-all-resource-groups.ps1
+++ remove-all-resource-groups.ps1
@@ -2,6 +2,6 @@ Get-AzSubscription -TenantId $TenantId | ForEach-Object {
     Set-AzContext -SubscriptionId $_.Id
     foreach ($rg in Get-AzResourceGroup)    {   
         Write-Host "Removing resource group: $($rg.ResourceGroupName)"
-        Remove-AzResourceGroup -Force
+        Remove-AzResourceGroup $rg -Force
     }
 }
