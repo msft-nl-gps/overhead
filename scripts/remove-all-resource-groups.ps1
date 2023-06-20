6f178a899c77695b74a988f6663e88889799656a
Bogdan Grozoiu <grozoiu@hotmail.com>
2023-06-20 14:45:55 +0200

    Accepting failure and announcing it at the end.

--- scripts/remove-all-resource-groups.ps1
+++ scripts/remove-all-resource-groups.ps1
@@ -2,12 +2,20 @@ Get-AzSubscription -TenantId $TenantId | ForEach-Object -Parallel {
     Set-AzContext -SubscriptionId $_.Id
     $RGs = Get-AzResourceGroup
     $RGs | ForEach-Object -Parallel {
-        try{
-            Write-Host "Trying to remove the resource group: $($_.ResourceGroupName)"
-            $_ | Remove-AzResourceGroup -Force -ErrorAction Continue
+        Write-Host "Trying to remove the resource group: $($_.ResourceGroupName)"
+        $_ | Remove-AzResourceGroup -Force -ErrorAction SilentlyContinue
+        
+        if (Get-AzResourceGroup -ResourceGroupName $_.ResourceGroupName -ErrorAction SilentlyContinue) {
+            Write-Warning "Failed to remove resource group: $($_.ResourceGroupName). The job will fail at the end."
+            $jobhasfailed = $true
         }
-        catch{
-            Write-Warning "Failed to remove resource group: $($_.ResourceGroupName)"
+        else{
+            Write-Host "Successfully removed resource group: $($_.ResourceGroupName)"
         }
+        
     }
+}
+
+if ($jobhasfailed) {
+    Write-Error "Failed to remove all resource groups. Re-run this workflow to try again."
 }
