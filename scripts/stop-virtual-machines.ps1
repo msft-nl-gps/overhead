e3b5799feca05e27ffe8a0c0d67be0781d023925
Bogdan Grozoiu <grozoiu@hotmail.com>
2023-06-20 13:02:09 +0200

    #6 #7 adding a wf to stop vms in parallel at 7PM

---
+++ scripts/stop-virtual-machines.ps1
@@ -0,0 +1,14 @@
+Get-AzSubscription -TenantId $TenantId | ForEach-Object -Parallel {
+    Set-AzContext -SubscriptionId $_.Id
+    $VMs = Get-AzVM
+    $VMs | ForEach-Object -Parallel {
+        try{
+            Write-Host "Trying to stop the VM: $($_.Name)"
+            Stop-AzVM $_ -Force
+            Write-Host "Successfully stopped the VM: $($_.Name)"
+        }
+        catch{
+            Write-Warning "Failed to stop VM: $($_.Name)"
+        }
+    }
+}
