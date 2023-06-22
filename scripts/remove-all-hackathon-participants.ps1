1d3a3e1dd423cbd47a83ebff2d9434fb7f36df09
Bogdan Grozoiu <grozoiu@hotmail.com>
2023-06-22 11:48:03 +0200

    experimenting with Access Token for SPNs

--- scripts/remove-all-hackathon-participants.ps1
+++ scripts/remove-all-hackathon-participants.ps1
@@ -1,1 +1,3 @@
 Write-Host "Removing all hackathon participants..."
+$authToken = Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com"
+Write-Host ('Access Token acquired! Length: [{0}] chars - Expires on: [{1}]' -f $authToken.Token.Length, $authToken.ExpiresOn)
