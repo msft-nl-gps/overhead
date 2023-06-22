48346acbcb0cc600e8e697fe5692ef54cfbcd4d1
Bogdan Grozoiu <grozoiu@hotmail.com>
2023-06-22 13:47:19 +0200

    #8 pre-flight check

--- scripts/remove-all-hackathon-participants.ps1
+++ scripts/remove-all-hackathon-participants.ps1
@@ -7,7 +7,30 @@ $User_resource = "users"
 
 $uri = "https://graph.microsoft.com/$graphApiVersion/$($User_resource)"
 Write-Verbose $uri
-$allUsers = Invoke-RestMethod -Uri $uri -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -Method Get
-$allUsers.value | ForEach-Object {
-    Write-Host ("User: [{0}] with id [{1}] and email address: [{2}]" -f $_.displayName, $_.id, $_.mail)
+$allAADUsers = Invoke-RestMethod -Uri $uri -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -Method Get
+
+$allUsers = $allAADUsers.value
+Write-Host "Processing [$($allUsers.Count)] users..."
+
+foreach ($aadUser in $allUsers)  {
+    Write-Host ("Processing user: [{0}] with id [{1}]..." -f $aadUser.displayName, $aadUser.id)
+    
+    switch -wildcard ($aadUser.userPrincipalName.ToLower()) {
+        $null { 
+            Write-Error "UPN cannot be null!"
+        }
+        'admin@mngenvmcap*' {
+            Write-Host ("User: [{0}] with UPN: [{1}] is an admin and will be skipped." -f $aadUser.displayName, $aadUser.userPrincipalName)
+        }
+        'ms-serviceaccount@mngenvmcap*' {
+            Write-Host ("User: [{0}] with UPN: [{1}] is an admin and will be skipped." -f $aadUser.displayName, $aadUser.userPrincipalName)
+        }
+        '*microsoft.com#ext#*'{
+            Write-Host ("User: [{0}] is a Microsoft employee and will be skipped." -f $aadUser.displayName)
+        }
+        Default {
+            Write-Warning ("Removing user: [{0}] with id [{1}] and UPN: [{2}]" -f $aadUser.displayName, $aadUser.id, $aadUser.userPrincipalName)
+            #Invoke-RestMethod -Uri $($uri + '/' + $aadUser.id) -Headers @{'Authorization'='Bearer '+ $authToken.token} -Method Delete
+        }
+    }
 }
