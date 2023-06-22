dfba16e89f31aa33860dda675b5f6a93274ba098
Bogdan Grozoiu <grozoiu@hotmail.com>
2023-06-22 11:59:29 +0200

    doing a get request #8

--- scripts/remove-all-hackathon-participants.ps1
+++ scripts/remove-all-hackathon-participants.ps1
@@ -1,3 +1,13 @@
 Write-Host "Removing all hackathon participants..."
 $authToken = Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com"
 Write-Host ('Access Token acquired! Length: [{0}] chars - Expires on: [{1}]' -f $authToken.Token.Length, $authToken.ExpiresOn)
+
+$graphApiVersion = "beta"
+$User_resource = "users"
+
+$uri = "https://graph.microsoft.com/$graphApiVersion/$($User_resource)"
+Write-Verbose $uri
+$allUsers = Invoke-RestMethod -Uri $uri -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -Method Get
+$allUsers.value | ForEach-Object {
+    Write-Host ("User: [{0}] with id [{1}] and email address: [{2}]" -f $_.displayName, $_.id, $_.mail)
+}
