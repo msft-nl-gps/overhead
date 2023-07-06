4a79ca3e5a3793be61fdfa09ad45ca7fc251c9f3
Bogdan Grozoiu <grozoiu@hotmail.com>
2023-07-06 15:49:40 +0200

    #8 small improvements

--- scripts/provision-hackathon-participants.ps1
+++ scripts/provision-hackathon-participants.ps1
@@ -1,5 +1,6 @@
 param(
-    [Parameter(Mandatory=$true)][securestring]$passwordPrefix    
+    [Parameter(Mandatory = $true)][securestring]$passwordPrefix,
+    [Parameter(Mandatory = $false)][string]$maxNumberOfParticipants = 25
 )
 
 Write-Host "Provisioning hackathon participants..."
@@ -19,54 +20,58 @@ $uriUser = "https://graph.microsoft.com/$graphApiVersion/$($userResource)"
 $roleAssignmentGroupId = "75a3feb5-db44-4442-9d71-82826c19c56f"
 # Pay attention to the backtick (`) in the URI below. This is required to escape the $ref character in the URI.
 $uriGroup = "https://graph.microsoft.com/$graphApiVersion/$($groupResource)/$($roleAssignmentGroupId)/members/" + "`$ref"
-$password =  $(ConvertFrom-SecureString $passwordPrefix -AsPlainText) + $(Get-Date -Format yyyyMMdd) + '!'
+$password = $(ConvertFrom-SecureString $passwordPrefix -AsPlainText) + $(Get-Date -Format yyyyMMdd) + '!'
 
 
-if(Invoke-RestMethod -Uri "https://graph.microsoft.com/$graphApiVersion/$($groupResource)" -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Get){
+if (Invoke-RestMethod -Uri "https://graph.microsoft.com/$graphApiVersion/$($groupResource)" -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Get) {
 
-    for ($i = 1; $i -le 25; $i++) {
+    for ($i = 1; $i -le $maxNumberOfParticipants; $i++) {
         Write-Output ("Processing user: [{0}] ..." -f $i)
         $body = @{
             accountEnabled    = $true
             displayName       = "Hackathon Participant $($i)"
-            mailNickname      = "hackathon-$($i)"
-            userPrincipalName = "hackathon-$($i)$domain"
+            mailNickname      = $("hackathon" + '-' + $i)
+            userPrincipalName = $("hackathon" + '-' + $i + $domain)
             passwordProfile   = @{
                 forceChangePasswordNextSignIn = $true
                 password                      = "$password"
             }
             usageLocation     = "NL"
-        }| ConvertTo-Json
+        }
 
-        $user = Invoke-RestMethod -Uri $uriUser -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $body
+        $user = Invoke-RestMethod -Uri $uriUser -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $($body | ConvertTo-Json)
         Write-Warning ("Created user: [{0}]" -f $user.userPrincipalName)
 
         $bodyUser = @{
             "@odata.id" = "https://graph.microsoft.com/$graphApiVersion/directoryObjects/$($user.id)"
-        } | ConvertTo-Json
+        } 
 
-        Invoke-RestMethod -Uri $uriGroup -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $bodyUser
+        Invoke-RestMethod -Uri $uriGroup -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $($bodyUser | ConvertTo-Json)
         Write-Warning ("User: [{0}] with id [{1}] and UPN: [{2}] added to group [{3}] for licensing and role assingment" -f $user.displayName, $user.id, $user.userPrincipalName, $roleAssignmentGroupId)         
     }
     $bodyRoleAssignment = @{    
-        action                = "AdminAssign"
-        justification         = "Assign Hackathon user permissions via PIM for the duration of the hackathon"
-        roleDefinitionId      = "62e90394-69f5-4237-9190-012177145e10"
-        directoryScopeId      = "/"
-        principalId           = $roleAssignmentGroupId
-        scheduleInfo          = @{
-            startDateTime     = "2023-07-15T00:00:00Z"
-            expiration        = @{
-                endDateTime   = "2023-07-16T00:00:00Z"
-                type          = "AfterDateTime"
-                }
+        action           = "AdminAssign"
+        justification    = "Assign Hackathon user permissions via PIM for the duration of the hackathon"
+        roleDefinitionId = "62e90394-69f5-4237-9190-012177145e10"
+        directoryScopeId = "/"
+        principalId      = $roleAssignmentGroupId
+        scheduleInfo     = @{
+            startDateTime = "2023-07-15T00:00:00Z"
+            expiration    = @{
+                endDateTime = "2023-07-16T00:00:00Z"
+                type        = "AfterDateTime"
             }
-    } | ConvertTo-Json -Depth 3
+        }
+    }
 
-    Invoke-RestMethod -Uri "https://graph.microsoft.com/$graphApiVersion/roleManagement/directory/roleEligibilityScheduleRequests" -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $bodyRoleAssignment
+    Invoke-RestMethod -Uri "https://graph.microsoft.com/$graphApiVersion/roleManagement/directory/roleEligibilityScheduleRequests" -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $($bodyRoleAssignment | ConvertTo-Json -Depth 3)
    
-
-}else{
+    $body = $null
+    $user = $null
+    $bodyUser = $null
+    $bodyRoleAssignment = $null
+}
+else {
     Write-Warning ("Provisioning has stopped. Group for licensing and permissions with id [{0}] could not be found." -f $roleAssignmentGroupId)
 }
 Write-Host "Done provisioning hackathon participants!"
