7725f0aae7a4453dc573b5a9ede8ddf9b94a9dfc
Joke Feije <jokeedelman@hotmail.com>
2023-07-06 16:23:57 +0200

    #8 Code cleanup

--- scripts/provision-hackathon-participants.ps1
+++ scripts/provision-hackathon-participants.ps1
@@ -3,50 +3,51 @@ param(
     [Parameter(Mandatory = $false)][string]$maxNumberOfParticipants = 25,
     [Parameter(Mandatory = $false)][string]$domain = '@mngenvmcap168626.onmicrosoft.com',
     [Parameter(Mandatory = $false)][string]$roleAssignmentGroupId = '75a3feb5-db44-4442-9d71-82826c19c56f',
-    [Parameter(Mandatory = $false)][string]$graphApiVersion = 'beta'
+    [Parameter(Mandatory = $false)][string]$graphApiVersion = 'beta',
+    [Parameter(Mandatory = $false)][string]$usageLocation = 'NL'
 )
 
 Write-Host "Provisioning hackathon participants..."
 Write-Host "Assign role and license using group membership..."
 
-
+# Get Authentication token for the graph API
+# Permissions needed: Directory.ReadWrite.All, RoleManagement.ReadWrite.Directory, RoleManagement.ReadWrite.Directory.AccessAsUser.All
 $authToken = Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com"
 Write-Host ('Access Token acquired! Length: [{0}] chars - Expires on: [{1}]' -f $authToken.Token.Length, $authToken.ExpiresOn)
 
-$userResource = "users"
-$groupResource = "groups"
-
-$uriUser = "https://graph.microsoft.com/$graphApiVersion/$($userResource)"
-
+#$userResource = "users"
+#$groupResource = "groups"
+$baseUriUser = "https://graph.microsoft.com/" + $graphApiVersion + "/users"
+$baseUriGroup = "https://graph.microsoft.com/" + $graphApiVersion + "/groups"
 # Pay attention to the backtick (`) in the URI below. This is required to escape the $ref character in the URI.
-$uriGroup = "https://graph.microsoft.com/$graphApiVersion/$($groupResource)/$($roleAssignmentGroupId)/members/" + "`$ref"
+$uriGroupMembers = $baseUriGroup + "/" + $roleAssignmentGroupId +"/members/" + "`$ref"
 $password = $(ConvertFrom-SecureString $passwordPrefix -AsPlainText) + $(Get-Date -Format yyyyMMdd) + '!'
 
 
-if (Invoke-RestMethod -Uri "https://graph.microsoft.com/$graphApiVersion/$($groupResource)" -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Get) {
+if (Invoke-RestMethod -Uri $baseUriGroup -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Get) {
 
     for ($i = 1; $i -le $maxNumberOfParticipants; $i++) {
         Write-Output ("Processing user: [{0}] ..." -f $i)
         $body = @{
             accountEnabled    = $true
-            displayName       = "Hackathon Participant $($i)"
+            displayName       = $("Hackathon Participant " + $i)
             mailNickname      = $("hackathon" + '-' + $i)
             userPrincipalName = $("hackathon" + '-' + $i + $domain)
             passwordProfile   = @{
                 forceChangePasswordNextSignIn = $true
                 password                      = "$password"
             }
-            usageLocation     = "NL"
+            usageLocation     = $usageLocation
         }
 
-        $user = Invoke-RestMethod -Uri $uriUser -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $($body | ConvertTo-Json)
+        $user = Invoke-RestMethod -Uri $baseUriUser -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $($body | ConvertTo-Json)
         Write-Warning ("Created user: [{0}]" -f $user.userPrincipalName)
 
         $bodyUser = @{
             "@odata.id" = "https://graph.microsoft.com/$graphApiVersion/directoryObjects/$($user.id)"
         } 
 
-        Invoke-RestMethod -Uri $uriGroup -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $($bodyUser | ConvertTo-Json)
+        Invoke-RestMethod -Uri $uriGroupMembers -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $($bodyUser | ConvertTo-Json)
         Write-Warning ("User: [{0}] with id [{1}] and UPN: [{2}] added to group [{3}] for licensing and role assingment" -f $user.displayName, $user.id, $user.userPrincipalName, $roleAssignmentGroupId)
         $body = $null
         $user = $null
@@ -68,7 +69,7 @@ if (Invoke-RestMethod -Uri "https://graph.microsoft.com/$graphApiVersion/$($grou
         }
     }
 
-    Invoke-RestMethod -Uri "https://graph.microsoft.com/$graphApiVersion/roleManagement/directory/roleEligibilityScheduleRequests" -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $($bodyRoleAssignment | ConvertTo-Json -Depth 3)
+    Invoke-RestMethod -Uri "https://graph.microsoft.com/" + $graphApiVersion + "/roleManagement/directory/roleEligibilityScheduleRequests" -Headers @{'Authorization' = 'Bearer ' + $authToken.token } -ContentType 'application/json' -Method Post -Body $($bodyRoleAssignment | ConvertTo-Json -Depth 3)
     $bodyRoleAssignment = $null
 }
 else {
